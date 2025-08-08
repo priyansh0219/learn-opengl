418ffde0f4362c9ddc718685343751d0b28ac216
Priyansh Yadav <priyanshy0219@gmail.com>
2025-08-08 13:25:26 +0530

    Module 4 Exercise 2

---
+++ exercises/4.3.exercise2/main.cpp
@@ -0,0 +1,208 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include "shader.hpp"
+
+#include <iostream>
+
+// #define LO_VERBOSE
+
+// clang-format off
+float vertices[] = {
+    // Positions          // Color            // Texture Coordinates
+    -0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,     // Bottom left
+     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   2.0f, 0.0f,     // Bottom right
+    -0.5f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 2.0f,     // Top Left
+     0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   2.0f, 2.0f,     // Top Right
+};
+
+unsigned int indices[] = {
+    0, 2, 3,
+    3, 1, 0
+};
+// clang-format on
+
+void glfw_frame_buffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+#ifdef LO_VERBOSE
+    std::cout << "Window resized to (" << width << ", " << height << ")" << std::endl;
+#endif
+}
+
+void process_input(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+#ifdef LO_VERBOSE
+        std::cout << "Esc key pressed. Closing Window...";
+#endif
+    }
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD." << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, glfw_frame_buffer_size_callback);
+
+    // * Set shader texture parameters
+
+    // Set texture border color
+    float borderColor[] = {1.0f, 1.0f, 0.0f, 1.0f}; // Yellow, maybe??
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+
+    // Set texture filtering algos
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    stbi_set_flip_vertically_on_load(true);
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("resources/textures/container.jpg", &width, &height, &nrChannels, 0);
+
+    unsigned int textures[2];
+    glGenTextures(2, textures);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cerr << "ERROR::TEXTURE::STBI_DATA_EMPTY for texture 1" << std::endl;
+    }
+    stbi_image_free(data);
+    data = nullptr;
+
+    width = height = nrChannels = 0;
+    data = stbi_load("resources/textures/awesomeface.png", &width, &height, &nrChannels, 0);
+
+    // Mirror Repeat Texture
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, textures[1]);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cerr << "ERROR::TEXTURE::STBI_DATA_EMPTY for texture 2" << std::endl;
+    }
+    stbi_image_free(data);
+    data = nullptr;
+
+    // Create the shader object
+    Shader shader("exercises/4.3.exercise2/shader.vs.glsl", "exercises/4.3.exercise2/shader.fs.glsl");
+
+    // Create vertex buffer array
+    unsigned int VBO = 0;  // buffer id
+    glGenBuffers(1, &VBO); // create 1 buffer and set the id in VBO
+
+    // Create Element buffer array
+    unsigned int EBO = 0;
+    glGenBuffers(1, &EBO);
+
+    // Create vertex attribute array
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    // First bind VAO. This is like the master state for our object.
+    glBindVertexArray(VAO);
+
+    // Bind and populate EBO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Bind the VBO (which binds it to VAO) and populate vertex data.
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Set the vertex attribute pointers.
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    // Unbind the VBO. This is allowed since call the glVertexAttribPointer registered this VBO as the VBO for active VAO.
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Unbind the VAO so that any call to some other VAO doesnt mistakenly modify this one.
+    glBindVertexArray(0);
+
+    // Unbind the EBO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // Uncomment this for wireframe mode.
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    shader.use();
+    shader.setInt("texture1", 0);
+    shader.setInt("texture2", 1);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // Process Input
+        process_input(window);
+
+        // Rendering commands
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Use shader program and draw triangles
+        shader.use();
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textures[0]);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, textures[1]);
+        glBindVertexArray(VAO); // bind the VAO to use. this is bound to the EBO we used earlier.
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Free up resource
+    glDeleteTextures(2, textures);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    shader.destroy();
+
+    glfwTerminate();
+    return 0;
+}
---
+++ exercises/4.3.exercise2/shader.cpp
@@ -0,0 +1,113 @@
+#include "shader.hpp"
+
+Shader::Shader(const char *vertexPath, const char *fragmentPath)
+{
+    // * 1. Retrieve the vertex/fragment source code from filepath
+    std::string vertexCode;
+    std::string fragmentCode;
+    std::ifstream vShaderFile;
+    std::ifstream fShaderFile;
+
+    // Ensure the ifstreams can throw errors
+    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+    try
+    {
+        // open files
+        vShaderFile.open(vertexPath);
+        fShaderFile.open(fragmentPath);
+        std::stringstream vShaderStream, fShaderStream;
+
+        // read file's buffer contents into streams
+        vShaderStream << vShaderFile.rdbuf();
+        fShaderStream << fShaderFile.rdbuf();
+
+        // close file handlers
+        vShaderFile.close();
+        fShaderFile.close();
+
+        // convert stream into string
+        vertexCode = vShaderStream.str();
+        fragmentCode = fShaderStream.str();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
+    }
+
+    const char *vShaderCode = vertexCode.c_str();
+    const char *fShaderCode = fragmentCode.c_str();
+
+    // * 2. Compile Shaders
+    unsigned int vertexShader, fragmentShader;
+    int success;
+    char infoLog[512];
+
+    // vertex shader
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vShaderCode, NULL);
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX_SHADER_COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+    }
+
+    // fragment shader
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fShaderCode, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT_SHADER_COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+    }
+
+    // shader program
+    ID = glCreateProgram();
+    glAttachShader(ID, vertexShader);
+    glAttachShader(ID, fragmentShader);
+    glLinkProgram(ID);
+    glGetProgramiv(ID, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(ID, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::SHADER_PROGRAM_LINKING_FAILED\n"
+                  << infoLog << std::endl;
+    }
+
+    // delete shaders. no longer necessary.
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+}
+
+void Shader::use()
+{
+    glUseProgram(ID);
+}
+
+void Shader::setBool(const std::string& name, bool value) const
+{
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+
+void Shader::setInt(const std::string& name, int value) const
+{
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string& name, float value) const
+{
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::destroy()
+{
+    glDeleteProgram(ID);
+}
+
